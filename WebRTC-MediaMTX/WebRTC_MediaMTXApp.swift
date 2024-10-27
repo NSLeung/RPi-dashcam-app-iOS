8129e979648f0730839fb45d90bcb45c1ba8ff46
Bjarne Seiffert <svenbjarne@me.com>
2024-10-27 11:34:24 -0500

    Initial Commit

---
+++ WebRTC-MediaMTX/WebRTC_MediaMTXApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebRTC_MediaMTXApp.swift
+//  WebRTC-MediaMTX
+//
+//  Created by Sven-Bjarne Seiffert on 27.10.24.
+//
+
+import SwiftUI
+
+@main
+struct WebRTC_MediaMTXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
