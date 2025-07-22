34042313f50ff3d9e6ece4d1d4034bf89591451f
Nayman Leung <naymansleung@gmail.com>
2025-07-22 12:28:13 -0700

    Add icon asset
    * Add static gateway of rear RPi Zero 2 W

--- WebRTC-MediaMTX/Config.swift
+++ WebRTC-MediaMTX/Config.swift
@@ -4,4 +4,4 @@ let defaultIceServers = ["stun:stun.l.google.com:19302"]
 
 let streamURL = URL(string: baseURL)!
 let streamBackchannelURL = URL(string: baseURL + "_backchannel")!
-let baseURL = "http://192.168.1.27:8889/cam"
+let baseURL = "http://10.42.0.1:8889/cam"
